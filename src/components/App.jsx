4d6c7ce59017e9c689c176ae888b14a377cbf00d
Andriy Karpishyn <makaron4eg@gmail.com>
2023-05-13 22:57:41 +0300

    Implemented Searchbar component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,17 @@
 import { Component } from 'react';
+import AppStyled from './AppStyled';
+import Searchbar from './Searchbar/Searchbar';
 
 export class App extends Component {
+  state = {};
+
+  onSubmit = searchQuery => {};
+
   render() {
-    return <></>;
+    return (
+      <AppStyled>
+        <Searchbar onSubmit={this.onSubmit} />
+      </AppStyled>
+    );
   }
 }
